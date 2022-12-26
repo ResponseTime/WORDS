555836c4788f8aabf46481f8f1c29f1aa0508c57
Looper <aayush.bhattacharjee2002@gmail.com>
2022-12-26 18:37:32 +0530

    API ready started working on the gui part

--- main.py
+++ main.py
@@ -2,20 +2,35 @@ import requests
 import os
 import tkinter as tk
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
-class Game:
-    pass
+class API:
+    url = "https://random-words5.p.rapidapi.com/getRandom"
+    headers = {
+        "X-RapidAPI-Key": os.getenv('KEY'),
+        "X-RapidAPI-Host": os.getenv('HOST')
+    }
+
+    def get_word(self):
+        return requests.request("GET", self.url, headers=self.headers).text
 
 
-url = "https://random-words5.p.rapidapi.com/getRandom"
+class Game(API):
+    m = tk.Tk(className='words')
+    m.geometry('960x960')
+    frame = tk.Frame(m)
+    frame.pack(expand=True, padx=50, pady=20)
+    button = tk.Button(frame, text='Submit', background='black', foreground='white', width=25,padx=10)
+    e1 = tk.Entry(frame,background='green')
+    e1.grid(row=1)
+    button.grid(row=2)
 
-headers = {
-    "X-RapidAPI-Key": os.getenv('KEY'),
-    "X-RapidAPI-Host": os.getenv('HOST')
-}
+    def start(self):
+        self.m.mainloop()
 
-response = requests.request("GET", url, headers=headers)
 
-print(response.text)
+if __name__ == '__main__':
+    G1 = Game()
+    G1.start()
